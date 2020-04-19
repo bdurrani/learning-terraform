b6964bb3da118a8676a07672018e09eecd44ec0d
Bilal Durrani <bilal33@hotmail.com>
2020-04-19 15:48:57 -0400

    first

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-085925f297f89fce1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
