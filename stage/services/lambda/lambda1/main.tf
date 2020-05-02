373c6900c993b6149158f2eb13929cdbecc2ec6a
Bilal Durrani <bilal33@hotmail.com>
2020-05-02 11:02:54 -0400

    lambda triggered by cloudwatch events

--- stage/services/lambda/lambda1/main.tf
+++ stage/services/lambda/lambda1/main.tf
@@ -5,6 +5,10 @@ provider "aws" {
   version = "~> 2.59.0"
 }
 
+locals {
+  function_name = "test_lambda"
+}
+
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = var.app_path
@@ -13,13 +17,16 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "test_lambda1" {
   filename      = "${path.module}/lambda.zip"
-  function_name = "test_lambda1"
-  role          = aws_iam_role.iam_for_lambda.arn
+  function_name = local.function_name
+  # use a role created in terraform
+  # role          = aws_iam_role.iam_for_lambda.arn
   # role             = var.aws_iam_role
+  # use an existing role
+  role             = "arn:aws:iam::885834442506:role/lambda_basic_execution"
   handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "nodejs12.x"
-  depends_on       = [aws_iam_role_policy_attachment.lambda_logs, aws_cloudwatch_log_group.example]
+  # depends_on       = [aws_iam_role_policy_attachment.lambda_logs, aws_cloudwatch_log_group.example]
 
   environment {
     variables = {
@@ -32,51 +39,74 @@ resource "aws_lambda_function" "test_lambda1" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "example" {
-  name = "/aws/lambda/test_lambda"
-  # name              = "/aws/lambda/${var.lambda_function_name}"
-  retention_in_days = 14
+
+resource "aws_cloudwatch_event_rule" "cw_event" {
+  name = "${local.function_name}_events"
+  # run every minute
+  schedule_expression = "cron(0/1 * * * ? *)"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action" : "sts:AssumeRole",
-        "Principal" : {
-          "Service" : "lambda.amazonaws.com"
-        },
-        "Effect" : "Allow",
-        "Sid" : "",
-      },
-    ],
-  })
+resource "aws_cloudwatch_event_target" "cw_event_target" {
+  rule      = aws_cloudwatch_event_rule.cw_event.name
+  target_id = local.function_name
+  arn       = aws_lambda_function.test_lambda1.arn
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
+resource "aws_lambda_permission" "lambda_permission_cw" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda1.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.cw_event.arn
 }
 
-resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda_logging"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
+# resource "aws_cloudwatch_log_group" "example" {
+#   name = "/aws/lambda/test_lambda"
+#   # name              = "/aws/lambda/${var.lambda_function_name}"
+#   retention_in_days = 14
+# }
 
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action" : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource" : "arn:aws:logs:*:*:*",
-        "Effect" : "Allow"
-      }
-    ]
-  })
-}
+# How to create a role for a lambda
+
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name = "iam_for_lambda"
+#   assume_role_policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Action" : "sts:AssumeRole",
+#         "Principal" : {
+#           "Service" : "lambda.amazonaws.com"
+#         },
+#         "Effect" : "Allow",
+#         "Sid" : "",
+#       },
+#     ],
+#   })
+# }
+
+# resource "aws_iam_policy" "lambda_logging" {
+#   name        = "lambda_logging"
+#   path        = "/"
+#   description = "IAM policy for logging from a lambda"
+
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Action" : [
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ],
+#         "Resource" : "arn:aws:logs:*:*:*",
+#         "Effect" : "Allow"
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "lambda_logs" {
+#   role       = aws_iam_role.iam_for_lambda.name
+#   policy_arn = aws_iam_policy.lambda_logging.arn
+# }
+
